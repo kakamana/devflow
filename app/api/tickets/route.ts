b91190fa7ec28c608b8d19c097bda73041406ff1
kakamana <kamig4u@gmail.com>
2025-10-29 09:44:00 +0400

    api tickets & its routes

---
+++ app/api/tickets/route.ts
@@ -0,0 +1,22 @@
+import tickets from "@/app/database";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json(tickets);
+}
+
+export async function POST(request: Request) {
+  const { name, status, type } = await request.json();
+
+  const newTicket = {
+    id: tickets.length + 1,
+    name,
+    status,
+    type,
+  };
+  tickets.push(newTicket);
+
+  console.log(newTicket);
+
+  return NextResponse.json(newTicket);
+}
