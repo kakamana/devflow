706c38a16806b34f383be878207ed9187498c55b
kakamana <kamig4u@gmail.com>
2025-10-31 11:57:47 +0400

    Implemented OAuth with social providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,6 +9,7 @@ import handleError from "@/lib/handlers/error";
 import { NotFoundError, ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
 import { api } from "@/lib/api";
+import { auth } from "@/auth";
 
 const questions = [
   {
@@ -51,21 +52,23 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
+// const test = async () => {
+//   try {
+//     return await api.users.getAll();
+//   } catch (error) {
+//     return handleError(error);
+//   }
+// };
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
-  console.log("Users:", users);
+  //const users = await test();
+  const session = await auth();
+
+  console.log("Session: ", session);
 
   const { query = "", filter = "" } = await searchParams;
 
