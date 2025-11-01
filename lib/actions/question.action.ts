5812fb9aebc2bd31da6ebbf28101aced3b17d0eb
kakamana <kamig4u@gmail.com>
2025-11-01 17:46:40 +0400

    implemented UI States like DataRender

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -263,6 +263,7 @@ export async function getQuestions(
   }
 
   try {
+    //throw new Error("Error");
     const totalQuestions = await Question.countDocuments(filterQuery);
 
     const questions = await Question.find(filterQuery)
